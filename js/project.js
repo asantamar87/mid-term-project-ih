dc1d4ef4deb1c89dd3656f0212344cad02924423
aldosantamaria <asantama@jaumebalmes.net>
2022-11-25 10:36:02 +0100

    Fetch API  finished.  Day, 25 Nov

--- js/project.js
+++ js/project.js
@@ -1,4 +1,3 @@
-       console.log("Estamos aquí");
   const getData = () => {
     fetch('https://jsonplaceholder.typicode.com/posts/')
       .then((response) => response.json())
@@ -11,18 +10,6 @@
          document.querySelector(".project-detail").innerText =  project.body
 
       })
-      // {
-      //   // let project = result[0]
-      //   //  console.log(project)
-
-      //   // let title = document.querySelector(".project-page-title")
-      //   // title.innerText = project.title;
-      //   // let body = document.querySelector(".project-detail")
-      //   // body.innerText = project.body;
-      // }
-      
-      // );
-
 
   }
 
